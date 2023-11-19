f235f3edbbcdc65d1089e0f6251360c4c90c05de
Sasha <sashashengyt@gmail.com>
2023-11-18 16:00:08 -0800

    loadenv
    commit with some multion code
    
    remove en

--- ex-1.py
+++ ex-1.py
@@ -3,12 +3,14 @@ import multion
 from multion import MultionToolSpec
 from langchain.llms.openai import OpenAI as LangchainOpenAI
 from langchain.agents import initialize_agent, AgentType
+from dotenv import load_dotenv
 
 from langchain.tools import StructuredTool
 import subprocess
 import datetime
 from openai import OpenAI
 
+load_dotenv()
 client = OpenAI()
 
 
@@ -37,15 +39,22 @@ CANNED_RESULT = """Save the location of Union Square, San Francisco to a list of
 Share the directions to Union Square, San Francisco with a contact."""
 
 
-def call_gpt_vision():
-    return CANNED_RESULT
+def call_gpt_vision(goal="Find the address of the dolores park"):
     messages = [
         {
             "role": "system",
             "content": [
                 {
                     "type": "text",
-                    "text": "You are a GPT4 Vision assitant bot - you will be given a screenshot of the iOS simulator, please generate a list of actions to perform, write each action on a newline",
+                    "text": """
+You are a GPT4 Vision assitant bot - you will be given a screenshot of the iOS simulator, and a goal. Please generate a list of unitized actions to perform, write each action on a newline. Return nothing but the list of actions. For example:
+Tap on the 'search' tab
+Tap on the 'search' bar on the top
+Write text 'Dolores Park'
+Tap on the 'search' button
+Tap on the first item in the result of search
+Swipe up 
+                    """,
                 },
             ],
         },
@@ -55,10 +64,7 @@ def call_gpt_vision():
                 {
                     "type": "text",
                     # "text": "According to the screenshot, what is the next step so that I can listen to the same song at the same time with a friend?",
-                    "text": """Give me some actions to perform based on the scresnshot. Write them similar to below:
-                    Write text "Union Square" in search bar
-                    Click on the "save" button
-                    """,
+                    "text": f"""Give me some actions to perform based on the screenshot. The goal is {goal}, be specific.""",
                 },
                 {
                     "type": "image_url",
@@ -97,45 +103,69 @@ agent = initialize_agent(
     verbose=True,
 )
 
-goals = call_gpt_vision()
-goals = goals.split("\n")
-print(call_gpt_vision())
+tasks = call_gpt_vision(goal="Save 'Mission Dolores Park' in Places Tab")
+tasks = tasks.split("\n")
+print(tasks)
+exit(1)
+tasks = [
+    "Tap on the 'Search' tab at the bottom right corner",
+    "Tap on the 'Search Wikipedia' bar at the top",
+    "Type 'Mission Dolores Park'",
+    "Tap on the 'Mission Dolores Park' item in the search results",
+    "Tap on the 'Save' icon, usually a star or bookmark symbol, to add to saved places",
+    "Tap on the 'Places' tab at the bottom to go to saved places",
+    "Verify that 'Mission Dolores Park' is listed under saved places",
+]
+print(tasks)
+
+# exit(1)
 
 MAESTRO_URL = "http://localhost:9999/interact"
 # pass the image to GPT to generate a list of example features to test?
 
 
+# Trying to get multion to work directly.
 PROMPT1 = f"""
-You are an AI Agent whose job is to use the search bar to enumerate all possible click actions. Here is the link to start from: {MAESTRO_URL}. DO NOT guess or make up any user information, instead ask user to provide any missing info. Do not use any placeholders.
+You are an AI Agent whose job is to use the search bar to enumerate all possible click actions. Here is the link to start from: {MAESTRO_URL}.
 
-You are given the high level goal of "{goals[0]}".
-
-To accomplish a goal, you can use the "space AI" capability to generate the commands.
+You are given the high level goal. To fulfill this goal, engage the AI-powered command function within Maestro to formulate the commands. First start by clicking in the text box that has the placeholder "Press ‘space’ for AI, or type commands…". Never click/type on the search button. Never write quotes like "
 
 Here's how to use the "space AI":
-* Start by entering a single space character, then enter the goal such as "click on the Share button".
-* Submit the prompt, wait for the results to come back, then press the submit button.
+* In the longest input text bar, initiate with a space character, followed by detailing the goal in the format "X Y (optional)Z". Replace "X" with one of below: inputText, TapOn, longPressOn, launchApp; Replace Y with an UI element from the screen to achieve the goal. Replace Z with any additional text required to achieve the goal.
+* Right after inputting "X Y (optional)Z", wait for the blue button to appear to the right of the text box.
+* Press enter to submit the command.
+
+DO NOT WRITE QUOTES LIKE ".
+
+For example, the goal of "given the screenshot, find the address of dolores park", we execute the following:
+1. in the longest input text bar (not the search button) write: " tap on search icon"
+2. wait 2 seconds
+3. select the same text bar and press enter to submit the command
+4. wait for 2 seconds until the new text appears
+5. click on the purple button to the right of the text bar to submit
+6. wait 2 seconds
+7. clear in the input box
+8. in the longest input text bar (not the search button) write: " inputtext dolores park"
+9. wait 2 seconds
+10. select the same text bar and press enter to submit the command
+11. wait for 2 seconds
+
+Goal: Given the screenshot, find the location of golden gate park
 """
 
-PROMPT2 = f"""
-You are an AI Agent with the objective to systematically explore and list all possible click actions using the search bar in the web-based tool. Begin at this URL: {MAESTRO_URL}. Remember, do not assume or fabricate any user data; always request the necessary information from the user directly. Avoid placeholders.
+for task in tasks:
+    PROMPT3 = f"""
+press " " (space) in the command bar, then stop immediately
+enter the text 'Tap on the 'Mission Dolores Park' item in the search results", then stop immediately
+press enter after the text you just entered, then stop immediately
+press the last submit button, then stop immediately
+    """
 
-You don't need to navigate to any other pages, everything you need is on the current page.
+    PROMPT = PROMPT3
+    print(PROMPT)
 
-Your primary goal is to "{goals[0]}".
-
-To fulfill this goal, engage the AI-powered command function within Maestro to formulate the commands.
-
-* In the command bar, initiate with a space character, followed by detailing the goal in the format "X on Y Z". Replace "Y" with a clickable element (button or link) that corresponds to the context presented on the left side of the interface. "X" should be a command from the set ["click", "type"], while "Z" represents any additional text required to achieve the goal.
-* After submitting the goal statement, patiently await the response, and then execute the provided commands by pressing the submit button.
-* If the "space AI" misinterprets the instruction or fails to generate a viable command, refine your instruction by providing more context or specificity about the clickable elements available on the screen.
-"""
-
-PROMPT = PROMPT2
-print(PROMPT)
-
-response = agent(
-    inputs={
-        "input": PROMPT,
-    }
-)
+    response = agent(
+        inputs={
+            "input": PROMPT,
+        }
+    )
