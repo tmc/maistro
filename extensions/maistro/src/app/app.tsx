f85e6b2aa2a0d83fb42d30a8a0c749e632413500
Travis Cline <travis.cline@gmail.com>
2023-11-18 17:26:35 -0800

    extension: iterate

--- extensions/maistro/src/app/app.tsx
+++ extensions/maistro/src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+import MaistroWelcome from './maistro-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="maistro" />
+      <MaistroWelcome title="maistro" />
     </div>
   );
 }
