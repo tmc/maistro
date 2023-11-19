88f1b67cc7c848e964f0debddab522faa1b756b5
Aditya Advani <aditya@bestparents.com>
2023-11-19 10:48:30 -0800

    feat: base maestro tests running, now need to make it work with gpt4v

--- lib/fs_helpers.py
+++ lib/fs_helpers.py
@@ -1,4 +1,20 @@
 import os
+import re
+
+
+def extract_path(text):
+    # Regular expression pattern for a Linux file path
+    pattern = r"/(?:[\w.-]+/)*(?:[\w.-]+|\.\.)/[\w/._-]+"
+
+    # Search for the pattern in the string
+    match = re.search(pattern, text)
+
+    if match:
+        path = match.group()
+        return path
+    else:
+        return None
+
 
 def get_script_cwd(filename=""):
     """
@@ -7,3 +23,28 @@ def get_script_cwd(filename=""):
     Slightly hacky but works good
     """
     return os.path.join(os.path.dirname(__file__), filename)
+
+
+def find_first_fext(directory, fext="png"):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if fnmatch.fnmatch(file, f"*.{fext}"):
+                return os.path.join(root, file)
+    return None
+
+
+def find_first_png(directory):
+    return find_first_fext(directory, "png")
+
+def find_first_log(directory):
+    return find_first_fext(directory, "log")
+
+def find_first_yaml(directory):
+    return find_first_fext(directory, "yaml")
+
+def find_first_xml(directory):
+    return find_first_fext(directory, "xml")
+
+def find_first_json(directory):
+    return find_first_fext(directory, "json")
+
--- run_maestro_test.py
+++ run_maestro_test.py
@@ -1,6 +1,16 @@
 import os
+import re
 import subprocess
-from lib.fs_helpers import get_script_cwd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from lib.fs_helpers import (
+    get_script_cwd,
+    extract_path,
+    find_first_fext,
+    find_first_png
+)
 
 OUTPUT_BASE_DIR = get_script_cwd("../output")
 
@@ -13,19 +23,44 @@ def run_maestro_test(run_id, test_filename, host='localhost'):
     output_file_path = os.path.join(OUTPUT_BASE_DIR, run_id, "output.xml")
     debug_output_path = os.path.join(OUTPUT_BASE_DIR, run_id, "debug")
     cmd = f"maestro --host {host} test {test_file_path} --format junit --output={output_file_path} --debug-output={debug_output_path}"
+    print(cmd)
     print(f"Running test {test_file_path}")
     result = subprocess.run(
         cmd,
         shell=True,
         capture_output=True,
     )
+    stdout = result.stdout.decode("utf-8")
+    stderr = result.stderr.decode("utf-8")
+
     print(result)
     print("STDOUT")
-    print(result.stdout.decode("utf-8"))
+    print(stdout)
     print("\n\nSTDERR")
-    print(result.stderr.decode("utf-8"))
+    print(stderr)
+
+    result_success = True if result.returncode == 0 else False
+
+    if "No running emulator found" in stderr:
+        return {"success": False, "message": "No running emulator found"}
+
+    if "Flow Failed" in stdout:
+        default_debug_folder = extract_path(stdout)
+        if default_debug_folder:  # moves the debug folder to a more readable name "latest"
+            cmd = f"rm {debug_output_path}/latest; cp -r {default_debug_folder} {debug_output_path}/latest"
+            result = subprocess.run(
+                cmd,
+                shell=True,
+                capture_output=True,
+            )
+
+            return {"success": False, "output_xml": output_file_path, "debug_folder": f"{debug_output_path}/latest"}
+
+    return {"success": result_success, "output_xml": output_file_path}
 
 
 if __name__ == "__main__":
-    run_maestro_test("run-1", "example-launch-success.yaml", host='172.27.80.1')
-    run_maestro_test("run-1", "example-fail.yaml", host='172.27.80.1')
+    # on windows the host is not localhost
+    host = os.environ.get("MAESTRO_HOST", "localhost")
+    print(run_maestro_test("run-1", "example-launch-success.yaml", host=host))
+    print(run_maestro_test("run-1", "example-fail.yaml", host=host))
